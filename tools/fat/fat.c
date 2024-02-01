8643ce4bccaf3635f85f0d49725638a9fe2cda41
EinDummkopf <songtaehwan@icloud.com>
2024-02-01 21:58:59 +0900

    error handler to boot

--- tools/fat/fat.c
+++ tools/fat/fat.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+typedef uint8_t bool;
+#define true 1;
+#define false 0;
 
 typedef struct {
     uint8_t BootJumpInstruction[3];
@@ -24,15 +29,50 @@ typedef struct {
     uint32_t VolumeId;
     uint8_t VolumeLabel[11];
     uint8_t SystemId[8];
-} BootSector;
+} __attribute__((__packed__)) BootSector;
 
-int main(int argc, char **argv){
+BootSector g_BootSector;
+uint8_t *g_Fat = NULL;
+
+bool readBootSector(FILE* disk){
+    return fread(&g_BootSector, sizeof(g_BootSector), 1, disk) > 0;
+}
+
+bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut){
+    bool ok = true;
+    ok = ok && (fseek(disk, lba * g_BootSector.BytePerSector, SEEK_SET) == 0);
+    ok = ok && (fread(bufferOut, g_BootSector.BytePerSector, count, disk) == count);
+    return ok;
+}
+
+bool readFat(FILE* disk){
+    g_Fat = (uint8_t*) malloc(g_BootSector.SectorsPerFat * g_BootSector.BytePerSector);
+    return readSectors(disk, g_BootSector.ReservedSectors, g_BootSector.SectorsPerFat, g_Fat);
+}
+
+int main(int argc, char *argv[]){
     if(argc < 3){
         printf("Syntax: %s <disk image> <file name>\n", argv[0]);
         return -1;
     }
 
-    FILE *disk = fopen(argv[1], "rb");
+    FILE* disk = fopen(argv[1], "rb");
+    if(!disk){
+        fprintf(stderr, "Cannot open disk image %s!", argv[1]);
+        return -1;
+    }
 
+    if(readBootSector(disk)){
+        fprintf(stderr, "Could not read boot sector!\n");
+        return -2;
+    }
+
+    if(!readFat(disk)){
+        fprintf(stderr, "Could not read FAT!\n");
+        free(g_Fat);
+        return -3;
+    }
+
+    free(g_Fat);
     return 0;
 }
