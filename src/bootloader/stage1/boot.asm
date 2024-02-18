84318194ba3d11b51252b02e0004cdcf54ef99ba
EinDummkopf <songtaehwan@icloud.com>
2024-02-18 16:12:25 +0000

    Edit kernel to stage2 in file

--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -105,7 +105,7 @@ start:
 	mov di, buffer
 
 .search_kernel:
-	mov si, file_kernel_bin
+	mov si, file_stage2_bin
 	mov cx, 11 ; compare up to 11 characters
 	push di
 	repe cmpsb
@@ -123,7 +123,7 @@ start:
 .found_kernel:
 	; di should have the address to the entry
 	mov ax, [di + 26] ; first logical cluster field (offset 26)
-	mov [kernel_cluster], ax
+	mov [stage2_cluster], ax
 
 	; load FAT from disk into memory
 	mov ax, [bdb_reserved_sectors]
@@ -139,8 +139,8 @@ start:
 
 .load_kernel_loop:
 	; read next cluster
-	mov ax, [kernel_cluster]
-	add ax, 31 ; first cluster = (kernel_cluster - 2) * sectors_per_cluster + start_sector
+	mov ax, [stage2_cluster]
+	add ax, 31 ; first cluster = (stage2_cluster - 2) * sectors_per_cluster + start_sector
 			   ; start sector = reserved + fats + root directory size = 1 + 18 + 14 = 33
 
 	mov cl, 1
@@ -150,7 +150,7 @@ start:
 	add bx, [bdb_bytes_per_sector]
 
 	; compute location of next cluster
-	mov ax, [kernel_cluster] 
+	mov ax, [stage2_cluster] 
 	mov cx, 3
 	mul cx
 	mov cx, 2
@@ -174,7 +174,7 @@ start:
 	cmp ax, 0x0FF8 ; end of chain
 	jae .read_finish
 
-	mov [kernel_cluster], ax
+	mov [stage2_cluster], ax
 	jmp .load_kernel_loop
 
 .read_finish:
@@ -202,7 +202,7 @@ floopy_error:
 	jmp wait_key_and_reboot
 
 kernel_not_found_error:
-	mov si, msg_kernel_not_found
+	mov si, msg_stage2_not_found
 	call puts
 	jmp wait_key_and_reboot
 
@@ -315,9 +315,9 @@ disk_reset:
 
 msg_loading: db "Loading...", ENDL,  0
 msg_read_failed: db "Read from disk failed!", ENDL, 0
-msg_kernel_not_found: db "STAGE2.BIN file not found!", ENDL, 0
-file_kernel_bin: db "STAGE2  BIN"
-kernel_cluster: dw 0
+msg_stage2_not_found: db "STAGE2.BIN file not found!", ENDL, 0
+file_stage2_bin: db "STAGE2  BIN"
+stage2_cluster: dw 0
 
 KERNEL_LOAD_SEGMENT equ 0x2000
 KERNEL_LOAD_OFFSET equ 0
